6ec9d6b409a8836a118dd6b3b6f9db0ef016a3e7
Lucchetto <lucchetto.tie@live.com>
2019-02-15 19:49:28 +0100

    Initial commit

---
+++ Sublime/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := SublimeTheme
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := SublimeTheme
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
+
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app/SublimeTheme
+
+include $(BUILD_PACKAGE)
